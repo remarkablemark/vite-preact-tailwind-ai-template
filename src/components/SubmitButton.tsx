05ee2ca9c9dd3159af4b8a26531c28b62761a77a
Mark <mark@remarkablemark.org>
2024-11-13 00:13:03 -0500

    feat(components): show Spinner when SubmitButton is disabled

--- src/components/SubmitButton.tsx
+++ src/components/SubmitButton.tsx
@@ -1,17 +1,14 @@
 import SendIcon from './SendIcon';
+import Spinner from './Spinner';
 
-interface Props {
-  disabled?: boolean;
-}
-
-export default function SubmitButton(props: Props) {
+export default function SubmitButton({ disabled = false }) {
   return (
     <button
-      class="focus-visible:ring-ring inline-flex h-9 w-9 items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium shadow transition-colors hover:bg-slate-900 hover:text-slate-100 focus-visible:outline-none focus-visible:ring-1 disabled:pointer-events-none disabled:opacity-50"
-      disabled={props.disabled}
+      class="focus-visible:ring-ring inline-flex h-9 w-9 items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium shadow transition-colors hover:border hover:border-blue-400 hover:bg-blue-50 focus-visible:outline-none focus-visible:ring-1 disabled:pointer-events-none disabled:opacity-50"
+      disabled={disabled}
       type="submit"
     >
-      <SendIcon />
+      {disabled ? <Spinner /> : <SendIcon />}
     </button>
   );
 }
