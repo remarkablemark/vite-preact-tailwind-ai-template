1837745d82583f1253c6ab734ed2750ab0366bf9
Mark <mark@remarkablemark.org>
2024-11-13 00:47:36 -0500

    feat(components): display message and button on error in Chat

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -11,26 +11,33 @@ export default function Chat() {
 
   // https://sdk.vercel.ai/docs/ai-sdk-ui/chatbot
   // https://sdk.vercel.ai/docs/reference/ai-sdk-ui/use-chat
-  const { handleInputChange, handleSubmit, input, isLoading, messages } =
-    useChat({
-      api: `${API_URL}/api/chat`,
-      streamProtocol: 'text',
+  const {
+    error,
+    handleInputChange,
+    handleSubmit,
+    input,
+    isLoading,
+    messages,
+    reload,
+  } = useChat({
+    api: `${API_URL}/api/chat`,
+    streamProtocol: 'text',
 
-      initialMessages: [
-        {
-          role: 'assistant',
-          content: 'How may I help you?',
-          id: ID,
-        },
-      ],
-
-      onError(error) {
-        if (DEV) {
-          // eslint-disable-next-line no-console
-          console.error(error);
-        }
+    initialMessages: [
+      {
+        role: 'assistant',
+        content: 'How may I help you?',
+        id: ID,
       },
-    });
+    ],
+
+    onError(error) {
+      if (DEV) {
+        // eslint-disable-next-line no-console
+        console.error(error);
+      }
+    },
+  });
 
   useEffect(() => {
     if (messagesRef.current) {
@@ -42,13 +49,32 @@ export default function Chat() {
     <section class="flex h-screen flex-col rounded-xl border shadow sm:h-[70vh]">
       <Header />
 
+      {error && (
+        <>
+          <p
+            class="mb-4 flex items-center justify-between rounded border border-red-200 bg-red-100 px-4 py-2 text-red-700"
+            role="alert"
+          >
+            <span>An error occurred.</span>
+
+            <button
+              class="w-16 rounded-md bg-white px-4 py-2 text-sm font-medium text-black shadow hover:bg-gray-50 focus-visible:outline-none focus-visible:ring-1"
+              type="button"
+              onClick={() => reload()}
+            >
+              Retry
+            </button>
+          </p>
+        </>
+      )}
+
       <div class="flex-1 space-y-4 overflow-auto px-6" ref={messagesRef}>
         {messages.map(({ role, content }) => (
           <p
             class={clsx(
               'flex w-max max-w-[75%] flex-col gap-2 rounded-lg px-3 py-2 text-sm',
-              (role === 'assistant' || role === 'system') && 'bg-slate-100',
-              role === 'user' && 'ml-auto bg-blue-600 text-slate-100',
+              (role === 'assistant' || role === 'system') && 'bg-gray-100',
+              role === 'user' && 'ml-auto bg-blue-600 text-gray-100',
             )}
           >
             {content}
